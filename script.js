60d0467734b75085645094835ce1bd72cee85c6b
S778153 <136680568+S778153@users.noreply.github.com>
2024-07-18 16:09:01 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.getElementById('menu-icon');
-    const nav = document.getElementById('nav');
-
-    menuIcon.addEventListener('click', function () {
-        nav.classList.toggle('active');
-        menuIcon.classList.toggle('open');
-    });
-});
